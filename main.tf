eb242954ed540b10b69ddfd8903789e826f76207
Andrea Cappelletti <andreacappelletti97@gmail.com>
2026-04-28 10:04:59 -0400

    Upgrade instance sizes

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "m5.xlarge"
+  instance_type = "m6i.2xlarge"
 
   root_block_device {
-    volume_size = 50
-    volume_type = "gp2"
+    volume_size = 100
+    volume_type = "gp3"
   }
 }
 
 resource "aws_db_instance" "main" {
   engine         = "postgres"
-  instance_class = "db.r5.large"
+  instance_class = "db.r5.xlarge"
   multi_az       = true
-  allocated_storage = 100
+  allocated_storage = 200
 }
